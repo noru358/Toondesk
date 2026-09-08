cc053aaa845a93023cdc2073ce8226da9df6cd4f
noru358 <dkfxmdortm@gmail.com>
2026-09-09 00:39:40 +0900

    add secure desktop preload bridge

---
+++ desktop/preload.cjs
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('toondeskDesktop', {
+  isDesktop: true,
+  openProject: () => ipcRenderer.invoke('toondesk:open-project'),
+  saveFile: payload => ipcRenderer.invoke('toondesk:save-file', payload),
+  takePendingProject: () => ipcRenderer.invoke('toondesk:take-pending-project'),
+  onOpenProject: callback => {
+    const handler = (_event, payload) => callback(payload);
+    ipcRenderer.on('toondesk:open-project-event', handler);
+    return () => ipcRenderer.removeListener('toondesk:open-project-event', handler);
+  }
+});
